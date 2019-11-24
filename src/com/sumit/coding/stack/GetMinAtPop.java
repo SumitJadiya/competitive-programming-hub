4328239fd3ee5111ff410f68ac19bbdc3dbd9158
sumit.jadiya <sumit.jadiya@appdirect.com>
2019-11-24 19:02:18 +0530

    Added code optimisation to GetMinAtPop file

--- src/com/sumit/coding/stack/GetMinAtPop.java
+++ src/com/sumit/coding/stack/GetMinAtPop.java
@@ -1,12 +1,10 @@
 package com.sumit.coding.stack;
 
-import java.util.Scanner;
 import java.util.Stack;
 
 public class GetMinAtPop {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int[] arr = {1, 6, 43, 1, 2, 0, 5};
 
         Stack<Integer> mys = _push(arr, arr.length);
@@ -14,21 +12,21 @@ public class GetMinAtPop {
         System.out.println();
     }
 
-    public static Stack<Integer> _push(int arr[], int n) {
+    private static Stack<Integer> _push(int[] value, int n) {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
             if (stack.isEmpty()) {
-                stack.push(arr[i]);
-            } else if (stack.peek() > arr[i]) {
-                stack.push(arr[i]);
+                stack.push(value[i]);
+            } else if (stack.peek() > value[i]) {
+                stack.push(value[i]);
             } else
                 stack.push(stack.peek());
         }
         return stack;
     }
 
-    static void _getMinAtPop(Stack<Integer> s) {
+    private static void _getMinAtPop(Stack<Integer> s) {
 
         while (!s.isEmpty())
             System.out.print(s.pop() + " ");
