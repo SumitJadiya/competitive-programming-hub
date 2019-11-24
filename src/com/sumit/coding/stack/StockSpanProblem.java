ba4e7ec3d94d52056ccabb7b996ff98f0824362b
sumit.jadiya <sumit.jadiya@appdirect.com>
2019-11-24 23:56:22 +0530

    Added code for Stock Span Problem using Stack

---
+++ src/com/sumit/coding/stack/StockSpanProblem.java
@@ -0,0 +1,32 @@
+package com.sumit.coding.stack;
+
+import java.util.Stack;
+
+public class StockSpanProblem {
+    public static void main(String[] args) {
+        int[] a = {100, 80, 60, 70, 60, 75, 85, 10, 4, 5, 90, 120, 80};
+        int n = a.length;
+        int[] s = new int[n];
+
+        new StockSpanProblem().calculateSpan(a, n, s);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(s[i] + " ");
+        }
+    }
+
+    private void calculateSpan(int[] price, int n, int[] S) {
+        // Your code here
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        S[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && (price[stack.peek()] <= price[i]))
+                stack.pop();
+
+            S[i] = (stack.isEmpty()) ? (i + 1) : (i - stack.peek());
+            stack.push(i);
+        }
+    }
+}
