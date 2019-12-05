071060c381216847ddb9a41a055620f1f17d731c
sumit.jadiya <sumit.jadiya@appdirect.com>
2019-12-05 21:51:16 +0530

    Added code to print largest value in each row of Tree

---
+++ src/com/sumit/coding/Tree/TreeNode.java
@@ -0,0 +1,36 @@
+package com.sumit.coding.Tree;
+
+public class TreeNode {
+
+    int key;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        key = x;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
--- src/com/sumit/coding/Tree/VerticalSumInBinaryTree.java
+++ src/com/sumit/coding/Tree/VerticalSumInBinaryTree.java
@@ -3,43 +3,6 @@ package com.sumit.coding.Tree;
 import java.util.Map;
 import java.util.TreeMap;
 
-class TreeNode {
-
-    private int key;
-    private TreeNode left;
-    private TreeNode right;
-
-    public TreeNode(int key) {
-        this.key = key;
-        this.left = null;
-        this.right = null;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public TreeNode getLeft() {
-        return left;
-    }
-
-    public void setLeft(TreeNode left) {
-        this.left = left;
-    }
-
-    public TreeNode getRight() {
-        return right;
-    }
-
-    public void setRight(TreeNode right) {
-        this.right = right;
-    }
-}
-
 // Class for Binary Tree
 class Tree {
 
