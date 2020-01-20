d7e926154e0edffcff143d7d49d240b8e7a4811c
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-01-20 19:51:48 +0530

    code to calculate anti-diagonal numbers

---
+++ src/com/sumit/coding/matrix/AntiDiagonalsProblem.java
@@ -0,0 +1,28 @@
+package com.sumit.coding.matrix;
+
+public class AntiDiagonalsProblem {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        AntiDiagonalsProblem antiDiagonalsProblem = new AntiDiagonalsProblem();
+        int[][] res = antiDiagonalsProblem.diagonal(A);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+        }
+    }
+
+    private int[][] diagonal(int[][] a) {
+        int[][] res = new int[a.length][];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (i + j == 0) {
+                    res[i][j] = a[i][j];
+                }
+            }
+        }
+        return res;
+    }
+}
