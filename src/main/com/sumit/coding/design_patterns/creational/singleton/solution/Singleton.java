2ac353fdb09a285763f1179ec898ea539ccf6c39
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-03-06 01:48:16 +0530

    Solution of Singleton Design pattern

--- src/main/com/sumit/coding/design_patterns/creational/singleton/solution/Singleton.java
+++ src/main/com/sumit/coding/design_patterns/creational/singleton/solution/Singleton.java
@@ -1,16 +1,4 @@
-package main.com.sumit.coding.design_patterns;
-
-/**
- * Public class that uses singleton class
- */
-public class SingletonDesignPattern {
-
-    public static void main(String[] args) {
-        Singleton singleton = Singleton.getInstance();
-        System.out.println(singleton);
-        System.out.println(singleton);
-    }
-}
+package main.com.sumit.coding.design_patterns.creational.singleton.solution;
 
 /**
  * Class that implements Singleton design Pattern
