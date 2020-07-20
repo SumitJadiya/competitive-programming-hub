bbdbe0e3a2b177b733ef2efd3b39c1000ce7d3a0
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-20 19:50:47 +0530

    longest common prefix

--- src/main/com/sumit/coding/strings/LongestCommonPrefix.java
+++ src/main/com/sumit/coding/strings/LongestCommonPrefix.java
@@ -1,12 +1,38 @@
 package main.com.sumit.coding.strings;
 
+/**
+ * URL : https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/
+ *
+ * Approach 1 : Horizontal Scanning [O(S) time , O(1) space]
+ *
+ * Approach 2 : Vertical Scanning [O(S) time , O(1) space]
+ */
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"};
-        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs));
+        String[] strs = {"flo","flower","flow","flight"};
+        System.out.println("Largest Common Prefix - " + new LongestCommonPrefix().longestCommonPrefix_approach1(strs));
+        System.out.println("Largest Common Prefix - " + new LongestCommonPrefix().longestCommonPrefix_approach2(strs));
+        System.out.println("Largest Common Prefix - " + new LongestCommonPrefix().longestCommonPrefix_approach2(strs));
     }
 
-    public String longestCommonPrefix(String[] strs) {
+    private String longestCommonPrefix_approach2(String[] strs) {
+
+        if(strs.length == 0)
+            return "";
+
+        for (int i=0; i<strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+
+            for (int j=1; j<strs.length; j++){
+                if(i==strs[j].length() || c!=strs[j].charAt(i))
+                    return strs[0].substring(0, i);
+            }
+        }
+
+        return strs[0];
+    }
+
+    public String longestCommonPrefix_approach1(String[] strs) {
         if (strs.length == 0) return "";
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++)
