db930cd8388a8956326817de26ee10afab8f8bc7
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-26 18:03:56 +0530

    Reverse word in string

--- src/main/com/sumit/coding/strings/ReverseWordsInStringProblem.java
+++ src/main/com/sumit/coding/strings/ReverseWordsInStringProblem.java
@@ -4,18 +4,22 @@ public class ReverseWordsInStringProblem {
 
     public static void main(String[] args) {
 
-        String s1 = "i.like.this.program.very.much";
-        String result = ReverseWordsInStringProblem.reverseWords(s1);
+        String s1 = "";
+        String result = ReverseWordsInStringProblem.reverseWords_approach1(s1);
         System.out.println("Reversed - " + result);
+
+        System.out.println("Reversed - " + ReverseWordsInStringProblem.reverseWords_approach2(s1));
     }
 
-    private static String reverseWords(String s1) {
+    private static String reverseWords_approach1(String s1) {
 
         String result = "";
-        String[] substrings = s1.split("\\.");
+        String[] substrings = s1.trim().split("\\s+");
+        if(substrings.length == 0)
+            return "";
 
         for (int i = substrings.length - 1; i >= 0; i--)
-            result += substrings[i] + ".";
+            result += substrings[i] + " " ;
 
         return result.substring(0, result.length() - 1);
     }
