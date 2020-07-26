bd459fc32152fc90b57d391ef219e0bd0b384021
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-26 18:01:22 +0530

    add digits

---
+++ src/main/com/sumit/coding/math/AddDigitsProblem.java
@@ -0,0 +1,16 @@
+package main.com.sumit.coding.math;
+
+/**
+ * URL : https://leetcode.com/articles/add-digits/
+ */
+public class AddDigitsProblem {
+
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(new AddDigitsProblem().addDigits(num));
+    }
+
+    public int addDigits(int num) {
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
+    }
+}
