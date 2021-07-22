007c4dde1d49a4300971f3d8e5728bfea80bb941
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-23 00:19:43 +0530

    add license Key formatting problem

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Competitive Programming Codebase
+----------------------------------
+
+* Google 
+    * Interview Process
+        - <a href="src/main/com/sumit/coding/google/InterviewProcess/LicenseKeyFormattingProblem.java">LicenseKeyFormattingProblem</a>
+
---
+++ src/main/com/sumit/coding/google/InterviewProcess/LicenseKeyFormattingProblem.java
@@ -0,0 +1,38 @@
+package main.com.sumit.coding.google.InterviewProcess;
+
+/*
+ * Problem URL :
+ * https://leetcode.com/problems/license-key-formatting/
+ *
+ * Input: s = "5F3Z-2e-9-w", k = 4
+ * Output: "5F3Z-2E9W"
+ * Explanation: The string s has been split into two parts, each part has 4 characters.
+ * Note that the two extra dashes are not needed and can be removed.
+ * */
+public class LicenseKeyFormattingProblem {
+
+    public static void main(String[] args) {
+        String s = "5F3Z-2e-9-w";
+        int k = 4;
+        System.out.println(licenseKeyFormatting(s, k));
+    }
+
+    public static String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if (ch == '-') continue;
+
+            if (count == k) {
+                sb.append("-");
+                count = 0;
+            }
+
+            sb.append(Character.toUpperCase(s.charAt(i)));
+            count++;
+        }
+
+        return sb.reverse().toString();
+    }
+}
