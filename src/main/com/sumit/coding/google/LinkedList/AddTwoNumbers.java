e171ccc58626501b4f690b39b81bf81c6c9ff81d
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-24 01:24:44 +0530

    improved code

--- src/main/com/sumit/coding/google/LinkedList/AddTwoNumbers.java
+++ src/main/com/sumit/coding/google/LinkedList/AddTwoNumbers.java
@@ -39,18 +39,16 @@ public class AddTwoNumbers {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int add = 0;
+        int carry = 0;
         ListNode preHead = new ListNode(0);
         ListNode dummy = preHead;
 
-        while (l1 != null || l2 != null || add != 0) {
-
-            int temp = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
-            add += temp;
-            dummy.next = new ListNode(add % 10);
+        while (l1 != null || l2 != null || carry != 0) {
+            carry += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            dummy.next = new ListNode(carry % 10);
             dummy = dummy.next;
 
-            add /= 10;
+            carry /= 10;
             l1 = (l1 == null ? null : l1.next);
             l2 = (l2 == null ? null : l2.next);
         }
