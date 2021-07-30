08fa15191841513b7659ee9f8c78ada8bf7f68b4
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-30 23:11:21 +0530

    three sum problem

---
+++ src/main/com/sumit/coding/google/arraysAndStrings/ThreeSumProblem.java
@@ -0,0 +1,39 @@
+package main.com.sumit.coding.google.arraysAndStrings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ThreeSumProblem {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet();
+        HashSet<Integer> dups = new HashSet();
+        Map<Integer, Integer> seen = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (dups.add(nums[i])) {
+                for (int j = i + 1; j < nums.length; ++j) {
+                    int complement = -nums[i] - nums[j];
+
+                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(triplet);
+                        result.add(triplet);
+                    }
+                    seen.put(nums[j], i);
+                }
+            }
+        }
+        return new ArrayList(result);
+    }
+}
