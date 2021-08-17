c77ad692f22c03ae98e8afd74cc38084e3f4c914
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-08-17 23:00:40 +0530

    add problem

---
+++ src/main/com/sumit/coding/dp/RemoveBoxesProblem.java
@@ -0,0 +1,44 @@
+package main.com.sumit.coding.dp;
+
+/*
+ * Problem URL : https://leetcode.com/problems/remove-boxes
+ * */
+public class RemoveBoxesProblem {
+
+    public static void main(String[] args) {
+        int[] boxes = {1, 3, 2, 2, 2, 3, 4, 3, 1};
+        System.out.println(new RemoveBoxesProblem().removeBoxes(boxes));
+    }
+
+    /*
+     * Time Complexity : O(n^4)
+     * Space Complexity : O(n^3)
+     * */
+    public int removeBoxes(int[] boxes) {
+        int[][][] dp = new int[100][100][100];
+        return calculatePoints(boxes, dp, 0, boxes.length - 1, 0);
+    }
+
+    public int calculatePoints(int[] boxes, int[][][] dp, int l, int r, int k) {
+        if (l > r)
+            return 0;
+
+        while (r > l && boxes[r] == boxes[r - 1]) {
+            r--;
+            k++;
+        }
+
+        if (dp[l][r][k] != 0)
+            return dp[l][r][k];
+
+        dp[l][r][k] = calculatePoints(boxes, dp, l, r - 1, 0) + (k + 1) * (k + 1);
+        for (int i = l; i < r; i++) {
+            if (boxes[i] == boxes[r]) {
+                dp[l][r][k] = Math.max(dp[l][r][k], calculatePoints(boxes, dp, l, i, k + 1)
+                        + calculatePoints(boxes, dp, i + 1, r - 1, 0));
+            }
+        }
+
+        return dp[l][r][k];
+    }
+}
