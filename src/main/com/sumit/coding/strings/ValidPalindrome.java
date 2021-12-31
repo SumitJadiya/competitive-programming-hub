571f77401a43a9916dcca1e769354ecc5842f772
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-31 19:54:56 +0530

    valid palindrome problem

--- src/main/com/sumit/coding/strings/ValidPalindrome.java
+++ src/main/com/sumit/coding/strings/ValidPalindrome.java
@@ -1,27 +1,37 @@
 package main.com.sumit.coding.strings;
 
 /**
- * URL : https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411/
+ * Valid Palindrome Problem
+ * URL : https://leetcode.com/problems/valid-palindrome/
+ *
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
  */
 public class ValidPalindrome {
 
     public static void main(String[] args) {
-        System.out.println(new ValidPalindrome().isPalindrome("Marge, let's \"[went].\" I await {news} telegram."));
+        System.out.println(new ValidPalindrome().isPalindrome("race a car"));
     }
 
     public boolean isPalindrome(String s) {
         StringBuilder builder = new StringBuilder();
 
         for (char ch : s.toCharArray())
-            if(Character.isLetterOrDigit(ch))
+            if (Character.isLetterOrDigit(ch))
                 builder.append(Character.toLowerCase(ch));
 
         return checkPalindrome(builder.toString());
     }
 
-    public boolean checkPalindrome(String s){
-        for (int i=0; i<s.length()/2; i++)
-            if(s.charAt(i) != s.charAt(s.length() - 1 - i)) return false;
+    /**
+    * Check if the string is palindrome
+    *
+    * Time : O(n), Space : constant
+    * */
+    public boolean checkPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++)
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) return false;
 
         return true;
     }
