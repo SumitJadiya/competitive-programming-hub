74f44fca2e2d580d4955bb0a57ad7bc5bbb5fce4
Sumit Jadiya <jadiyaskj@gmail.com>
2022-07-21 20:53:43 +0530

    Streams example

---
+++ src/main/com/sumit/coding/java8/streams/User.java
@@ -0,0 +1,42 @@
+package main.com.sumit.coding.java8.streams;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
+    }
+}
