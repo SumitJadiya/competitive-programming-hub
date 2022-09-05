e9b2e49df9bb43d5f95b1f6b8a169b4e3f987d0c
Sumit Jadiya <jadiyaskj@gmail.com>
2022-09-05 10:14:27 +0530

    complement of base 10 integer

--- src/main/com/sumit/coding/leetcode/Oct20/Problem05.java
+++ src/main/com/sumit/coding/leetcode/Oct20/Problem05.java
@@ -1,33 +1,26 @@
 package main.com.sumit.coding.leetcode.Oct20;
 
 /*
-* URL
-* https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3484/
-* */
+ * URL
+ * https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3484/
+ * */
 public class Problem05 {
     public static void main(String[] args) {
         System.out.println(new Problem05().bitwiseComplement(5));
     }
 
-    public int bitwiseComplement(int N) {
+    public int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
 
-        if (N==0) return 1;
+        // edge case
+        if (n == 0) return 1;
 
-        int count = 0;
-        int oldNum = N;
-        int num = 0;
-
-        while(N>0){
-            N>>=1;
-            count++;
+        while (m != 0) {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
         }
 
-        while(count>0){
-            num = num <<1;
-            num = num|1;
-            count--;
-        }
-
-        return num ^ oldNum;
+        return (~n) & mask;
     }
 }
---
+++ src/main/com/sumit/coding/math/ComplementOfBaseTenProblem.java
@@ -0,0 +1,30 @@
+package main.com.sumit.coding.math;
+
+/*
+ * Problem URL : https://leetcode.com/problems/complement-of-base-10-integer/
+ *
+ * Input: n = 10
+ * Output: 5
+ * Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
+ * */
+public class ComplementOfBaseTenProblem {
+
+    public static void main(String[] args) {
+        System.out.println(new ComplementOfBaseTenProblem().bitwiseComplement(10));
+    }
+
+    public int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+
+        // edge case
+        if (n == 0) return 1;
+
+        while (m != 0) {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+
+        return (~n) & mask;
+    }
+}
