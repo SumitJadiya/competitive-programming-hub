46306c404aa8bd6540fc0050dd711153b5407fda
Sumit Jadiya <jadiyaskj@gmail.com>
2022-09-21 11:38:53 +0530

    move zeroes

--- src/main/com/sumit/coding/arrays/MoveZeroesProblem.java
+++ src/main/com/sumit/coding/arrays/MoveZeroesProblem.java
@@ -1,21 +1,22 @@
 package main.com.sumit.coding.arrays;
 
 /*
- *
- * Problem URL
- * https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/
- *
+ * Move Zeroes
+ * Problem URL : https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/
  * */
 public class MoveZeroesProblem {
 
     public static void main(String[] args) {
-        int[] arr = {2, 1};
-        new MoveZeroesProblem().moveZeroes(arr);
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
+        int[] arr = {0, 2, 0, 1};
+        moveZeroes(arr);
+
+        for (int j : arr) System.out.print(j + " ");
     }
 
-    public void moveZeroes(int[] nums) {
+    /*
+     * Time Complexity : O(n)
+     * */
+    private static void moveZeroes(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0)
