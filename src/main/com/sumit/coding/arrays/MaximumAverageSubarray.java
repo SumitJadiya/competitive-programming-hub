4feb0d23525c5b7a15636fbd4c19f4c977ccef29
Sumit Jadiya <jadiyaskj@gmail.com>
2023-01-24 19:03:13 +0530

    approach using java8

--- src/main/com/sumit/coding/arrays/MaximumAverageSubarray.java
+++ src/main/com/sumit/coding/arrays/MaximumAverageSubarray.java
@@ -1,5 +1,7 @@
 package main.com.sumit.coding.arrays;
 
+import java.util.Arrays;
+
 /*
  * URL - https://leetcode.com/problems/maximum-average-subarray-i/description/
  * */
@@ -9,16 +11,28 @@ public class MaximumAverageSubarray {
         int[] nums = {1, 12, -5, -6, 50, 3};
         int k = 4;
         System.out.println("result is = " + new MaximumAverageSubarray().findMaxAverage(nums, k));
-
+        System.out.println("result is = " + new MaximumAverageSubarray().findMaxAverage__2(nums, k));
         nums = new int[]{4, 2, 1, 3, 3};
         k = 2;
         System.out.println("result is = " + new MaximumAverageSubarray().findMaxAverage(nums, k));
+        System.out.println("result is = " + new MaximumAverageSubarray().findMaxAverage__2(nums, k));
     }
 
     public double findMaxAverage(int[] nums, int k) {
         double sum = 0;
         for (int i = 0; i < k; i++)
             sum += nums[i];
+        double res = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            res = Math.max(res, sum);
+        }
+        return res / k;
+    }
+
+    public double findMaxAverage__2(int[] nums, int k) {
+        double sum = Arrays.stream(nums, 0, k).sum();
+
         double res = sum;
         for (int i = k; i < nums.length; i++) {
             sum += nums[i] - nums[i - k];
