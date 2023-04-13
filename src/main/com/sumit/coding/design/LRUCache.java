832e60bd44b57f53916efdfe8f1fc9798cdfe245
Sumit Jadiya <jadiyaskj@gmail.com>
2023-04-14 03:06:50 +0530

    implement LRU Cache

---
+++ src/main/com/sumit/coding/design/LRUCache.java
@@ -0,0 +1,105 @@
+package main.com.sumit.coding.design;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private void removeNode(DLinkedNode node) {
+        DLinkedNode prevNode = node.prev;
+        DLinkedNode nextNode = node.next;
+
+        node.prev.next = nextNode;
+        node.next.prev = prevNode;
+    }
+
+    private void addNode(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private DLinkedNode popTail() {
+        DLinkedNode prevTail = tail.prev;
+        removeNode(prevTail);
+
+        return prevTail;
+    }
+
+    int capacity;
+    int size;
+    DLinkedNode head;
+    DLinkedNode tail;
+    Map<Integer, DLinkedNode> cache = new HashMap<>();
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) return -1;
+
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            addNode(newNode);
+            cache.put(key, newNode);
+
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        System.out.println(lRUCache.get(1));
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        System.out.println(lRUCache.get(2));
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        System.out.println(lRUCache.get(1));
+        System.out.println(lRUCache.get(3));
+        System.out.println(lRUCache.get(4));
+    }
+}
