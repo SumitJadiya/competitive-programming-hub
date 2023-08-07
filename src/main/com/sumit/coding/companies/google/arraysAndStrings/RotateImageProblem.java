c89209adfa42ec5165b4b028fc8967ea8f8b0f69
Sumit Jadiya <jadiyaskj@gmail.com>
2023-08-07 22:30:02 +0530

    rotate image another approach

--- src/main/com/sumit/coding/companies/google/arraysAndStrings/RotateImageProblem.java
+++ src/main/com/sumit/coding/companies/google/arraysAndStrings/RotateImageProblem.java
@@ -10,6 +10,11 @@ public class RotateImageProblem {
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         new RotateImageProblem().rotate(matrix);
+
+        System.out.println("-----");
+
+        matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        new RotateImageProblem().rotate_secondApproach(matrix);
     }
 
     /*
@@ -52,6 +57,22 @@ public class RotateImageProblem {
             }
             System.out.println();
         }
+    }
+
+    public void rotate_secondApproach(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        print(matrix);
     }
 }
 
