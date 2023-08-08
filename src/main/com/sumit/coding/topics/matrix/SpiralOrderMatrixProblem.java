d094ff9d0f4b3120f973a6db4172732208b9d060
Sumit Jadiya <jadiyaskj@gmail.com>
2023-08-08 22:45:51 +0530

    fix spiral matrix

--- src/main/com/sumit/coding/topics/matrix/SpiralOrderMatrixProblem.java
+++ src/main/com/sumit/coding/topics/matrix/SpiralOrderMatrixProblem.java
@@ -1,58 +1,62 @@
 package main.com.sumit.coding.topics.matrix;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/*
+ * URL - https://leetcode.com/problems/spiral-matrix
+ * */
 public class SpiralOrderMatrixProblem {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(Arrays.deepToString(matrix));
 
-        list.add(1);
-        list.add(2);
-//        list.add(3);
-        lists.add(list);
-        list = new ArrayList<>();
-        list.add(4);
-        list.add(5);
-//        list.add(6);
-        lists.add(list);
-        list = new ArrayList<>();
-        list.add(7);
-        list.add(8);
-//        list.add(9);
-        lists.add(list);
-
-        list = spiralOrder(lists);
+        List<Integer> list = spiralOrder(matrix);
         System.out.println(list);
-        System.out.println(lists);
     }
 
-    private static ArrayList<Integer> spiralOrder(final ArrayList<ArrayList<Integer>> A) {
-        ArrayList<Integer> al = new ArrayList<>();
-        int l = 0, n = A.get(0).size(); // column
-        int k = 0, m = A.size(); // row
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
 
-        while (l < n && k < m) {
-            for (int i = l; i < n; i++)
-                al.add(A.get(k).get(i));
-            k++;
-            for (int i = k; i < m; i++)
-                al.add(A.get(i).get(n - 1));
-            n--;
-            if (k < m) {
-                for (int i = n - 1; i >= l; i--)
-                    al.add(A.get(m - 1).get(i));
-                m--;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int up = 0;
+        int left = 0;
+        int right = columns - 1;
+        int down = rows - 1;
+
+        while (result.size() < rows * columns) {
+            // Traverse from left to right.
+            for (int col = left; col <= right; col++)
+                result.add(matrix[up][col]);
+
+            // Traverse downwards.
+            for (int row = up + 1; row <= down; row++)
+                result.add(matrix[row][right]);
+
+            // Make sure we are now on a different row.
+            if (up != down) {
+                // Traverse from right to left.
+                for (int col = right - 1; col >= left; col--)
+                    result.add(matrix[down][col]);
             }
-            if (l < n) {
-                for (int i = m - 1; i >= k; i--)
-                    al.add(A.get(i).get(l));
-                l++;
+
+            // Make sure we are now on a different column.
+            if (left != right) {
+                // Traverse upwards.
+                for (int row = down - 1; row > up; row--)
+                    result.add(matrix[row][left]);
             }
+
+            left++;
+            right--;
+            up++;
+            down--;
         }
 
-        return al;
+        return result;
     }
-
 }
