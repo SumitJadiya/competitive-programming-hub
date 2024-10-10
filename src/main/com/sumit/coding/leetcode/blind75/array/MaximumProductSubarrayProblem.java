19d4377df8601f3945363c2efbb4e83e81aa899f
Sumit Jadiya <jadiyaskj@gmail.com>
2024-10-10 15:39:44 +0530

    add complexity to maximum product sub array problem

--- src/main/com/sumit/coding/leetcode/blind75/array/MaximumProductSubarrayProblem.java
+++ src/main/com/sumit/coding/leetcode/blind75/array/MaximumProductSubarrayProblem.java
@@ -1,5 +1,12 @@
 package com.sumit.coding.leetcode.blind75.array;
 
+/*
+ * Problem URL : https://leetcode.com/problems/maximum-product-subarray/description/
+ *
+ * Input: nums = [2,3,-2,4]
+ * Output: 6
+ * Explanation: [2,3] has the largest product 6.
+ * */
 public class MaximumProductSubarrayProblem {
 
     public static void main(String[] args) {
@@ -14,6 +21,10 @@ public class MaximumProductSubarrayProblem {
 
     }
 
+    /*
+     * Time Complexity : O(N)
+     * Space Complexity : O(1)
+     * */
     public int maxProduct(int[] nums) {
         int maxProduct = nums[0];
         int minProduct = nums[0];
