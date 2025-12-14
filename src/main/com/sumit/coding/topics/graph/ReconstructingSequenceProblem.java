f5b847c15ecc06de99206eb755f4f2d29e90242f
Sumit Jadiya <jadiyaskj@gmail.com>
2025-12-14 21:07:13 +0530

    ++ fix

--- src/main/com/sumit/coding/topics/graph/ReconstructingSequenceProblem.java
+++ src/main/com/sumit/coding/topics/graph/ReconstructingSequenceProblem.java
@@ -5,33 +5,62 @@ import java.util.*;
 public class ReconstructingSequenceProblem {
 
     public static boolean sequenceReconstruction(List<Integer> originals, List<List<Integer>> seqs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int n = originals.size();
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
 
-        for (int original : originals)
-            graph.put(original, new ArrayList<>());
+        for (int i = 1; i <= n; i++)
+            graph.put(i, new HashSet<>());
 
-        for (List<Integer> seq : seqs)
-            graph.get(seq.get(0)).add(seq.get(1));
+        for (List<Integer> seq : seqs) {
+            for (int i = 0; i < seq.size() - 1; i++) {
+                int earlyNum = seq.get(i), lateNum = seq.get(i + 1);
 
-        topoSort(graph);
-        return false;
+                graph.get(earlyNum).add(lateNum);
+            }
+        }
+        return topoSort(graph, originals);
     }
 
 
-    private static List<Integer> topoSort(Map<Integer, List<Integer>> graph) {
+    private static boolean topoSort(Map<Integer, Set<Integer>> graph, List<Integer> originals) {
         Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> reconstructed = new ArrayList<>();
         Map<Integer, Integer> inDegree = findInDegree(graph);
 
         inDegree.forEach((key, value) -> {
-            if (value == 0)
-                queue.add(key);
+            if (value == 0) queue.add(key);
         });
 
-        return null;
+        while (!queue.isEmpty()) {
+            if (queue.size() > 1) return false;
+
+            Integer node = queue.poll();
+            reconstructed.add(node);
+
+            for (Integer neighbor : graph.get(node)) {
+                inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+
+                if (inDegree.get(neighbor) == 0)
+                    queue.add(neighbor);
+            }
+        }
+
+        return originals.equals(reconstructed);
     }
 
-    private static Map<Integer, Integer> findInDegree(Map<Integer, List<Integer>> graph) {
-        return null;
+    private static Map<Integer, Integer> findInDegree(Map<Integer, Set<Integer>> graph) {
+        Map<Integer, Integer> indegree = new HashMap<>();
+
+        graph.keySet().forEach(node -> {
+            indegree.put(node, 0);
+        });
+
+        graph.entrySet().forEach(entry -> {
+            for (Integer neighbor : entry.getValue())
+                indegree.put(neighbor, indegree.get(neighbor) + 1);
+        });
+
+        return indegree;
     }
 
     public static void main(String[] args) {
